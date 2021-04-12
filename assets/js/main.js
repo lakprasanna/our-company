f74bba1af2d60f8194e246b4b4f9799a81639090
lakshmi <lakprasanna314@gmail.com>
2021-04-12 12:32:32 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+   $(document).ready(function () {
+            var previousScroll = 0;
+            $(window).scroll(function () {
+                var currentScroll = $(this).scrollTop();
+                if (currentScroll < 100) {
+                    showTopNav();
+                } else if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()) {
+                    if (currentScroll > previousScroll) {
+                        hideNav();
+                    } else {
+                        showNav();
+                    }
+                    previousScroll = currentScroll;
+                }
+            });
+            function hideNav() {
+                $(".navbar").removeClass("is-visible").addClass("is-hidden");
+            }
+            function showNav() {
+                $(".navbar").removeClass("is-hidden").addClass("is-visible").addClass("scrolling");
+            }
+
+
+
+
+
+
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > 100) {
+                  $('.back-to-top').fadeIn('slow');
+                } else {
+                  $('.back-to-top').fadeOut('slow');
+                }
+              });
+            
+              $('.back-to-top').click(function() {
+                $('html, body').animate({
+                  scrollTop: 0
+                }, 1500, 'easeInOutExpo');
+                return false;
+              });
+            
+
+   // Porfolio isotope and filter
+   $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+      aos_init();
+    
+    });
+  });
+
+
+        });
+
+
+       
